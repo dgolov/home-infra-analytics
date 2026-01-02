778fb744d934d30332bb4f68f516a6880fff1132
dgolov <dgolov@icloud.com>
2026-01-02 23:53:59 +0300

    added logging

--- api/config.py
+++ api/config.py
@@ -1,8 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+import logging
+import sys
+
+
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
 
 class Settings(BaseSettings):
-    app_name: str = "FastAPI"
+    log_level: str = "DEBUG"
+    log_path: str = "app.log"
+
+    app_name: str = "InfraAnalyticsAPI"
     debug: bool = False
 
     clickhouse_host: str
@@ -19,6 +29,27 @@ class Settings(BaseSettings):
     @property
     def clickhouse_url(self) -> str:
         return f"http://{self.clickhouse_host}:{self.clickhouse_port}"
+
+
+def setup_logging(log_level: str = "DEBUG", log_file: str = "app.log") -> None:
+    """ Настройки логирования
+    :param log_level:
+    :param log_file:
+    :return:
+    """
+    level = getattr(logging, log_level.upper(), logging.DEBUG)
+
+    handlers = [
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(log_file)
+    ]
+
+    logging.basicConfig(
+        level=level,
+        format=LOG_FORMAT,
+        datefmt=LOG_DATE_FORMAT,
+        handlers=handlers
+    )
 
 
 settings = Settings()
