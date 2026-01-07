2247d9354f5c694279ca4bb89209de8986bf3c67
dgolov <dgolov@icloud.com>
2026-01-07 15:41:01 +0300

    added docstring

--- api/dependencies.py
+++ api/dependencies.py
@@ -5,12 +5,24 @@ from core.db import MetricsReadRepository, MetricsWriteRepository
 
 
 def get_ch_client(request: Request) -> ChClient:
+    """ Get clickhouse client
+    :param request:
+    :return:
+    """
     return request.app.state.ch_client
 
 
 def get_write_repository(request: Request) -> MetricsWriteRepository:
+    """ Get repository for write data to clickhouse
+    :param request:
+    :return:
+    """
     return MetricsWriteRepository(ch=request.app.state.ch_client)
 
 
 def get_read_repository(request: Request) -> MetricsReadRepository:
+    """ Get repository for read data from clickhouse
+    :param request:
+    :return:
+    """
     return MetricsReadRepository(ch=request.app.state.ch_client)
