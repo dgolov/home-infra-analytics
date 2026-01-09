a7a285831558bf3042c5f193871346508ae3cacb
dgolov <dmitriy.evgenyevich@gmail.com>
2026-01-09 22:04:33 +0300

    upd init-config.sh

--- clickhouse/init-config.sh
+++ clickhouse/init-config.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 set -e
 
-if [ ! -f /etc/clickhouse-server/config.d/cluster.xml ]; then
-    echo "Generating cluster.xml from template..."
-    envsubst < /etc/clickhouse-server/config.d/cluster.xml.template \
-        > /etc/clickhouse-server/config.d/cluster.xml
-    echo "=== GENERATED cluster.xml ==="
-    cat /etc/clickhouse-server/config.d/cluster.xml
-fi
+generate_from_template() {
+    local template="$1"
+    local target="$2"
+    local description="$3"
 
-if [ -f /etc/clickhouse-server/users.d/infra_user.xml.template ] && \
-   [ ! -f /etc/clickhouse-server/users.d/infra_user.xml ]; then
-    echo "Generating infra_user.xml from template..."
-    envsubst < /etc/clickhouse-server/users.d/infra_user.xml.template \
-        > /etc/clickhouse-server/users.d/infra_user.xml
-    echo "=== GENERATED infra_user.xml ==="
-    cat /etc/clickhouse-server/users.d/infra_user.xml
-fi
+    if [ -f "$template" ] && [ ! -f "$target" ]; then
+        echo "Generating $description from template..."
+        envsubst < "$template" > "$target"
+        echo "=== GENERATED $description ==="
+        cat "$target"
+    fi
+}
 
+generate_from_template "/etc/clickhouse-server/config.d/cluster.xml.template" \
+                       "/etc/clickhouse-server/config.d/cluster.xml" \
+                       "cluster.xml"
 
-if [ -f /etc/clickhouse-server/config.d/listen_hosts.xml.template ] && \
-   [ ! -f /etc/clickhouse-server/config.d/listen_hosts.xml ]; then
-    echo "Generating listen_hosts.xml from template..."
-    envsubst < /etc/clickhouse-server/config.d/listen_hosts.xml.template \
-        > /etc/clickhouse-server/config.d/listen_hosts.xml
-    echo "=== GENERATED listen_hosts.xml ==="
-    cat /etc/clickhouse-server/config.d/listen_hosts.xml
-fi
+generate_from_template "/etc/clickhouse-server/users.d/infra_user.xml.template" \
+                       "/etc/clickhouse-server/users.d/infra_user.xml" \
+                       "infra_user.xml"
+
+generate_from_template "/etc/clickhouse-server/config.d/listen_hosts.xml.template" \
+                       "/etc/clickhouse-server/config.d/listen_hosts.xml" \
+                       "listen_hosts.xml"
 
 exec /entrypoint.sh
