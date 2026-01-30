8bfccc0ea22f2c05fdd0b7adcf712f8c39a1cf19
grolov <dmitriy.evgenyevich@gmail.com>
2026-01-31 00:22:56 +0300

    refactoring

--- collectors/node_collector.py
+++ collectors/node_collector.py
@@ -14,105 +14,114 @@ logger = logging.getLogger(__name__)
 VM = socket.gethostname()
 
 
-def collect_network_metrics(metrics: List[Dict[str, Any]]) -> None:
-    net = psutil.net_io_counters()
-    metrics.extend([
-        {
+class Collector:
+    def __init__(self) -> None:
+        self.metrics: List[Dict[str, Any]] = []
+
+    def collect_cpu_metrics(self) -> None:
+        self.metrics.append(
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "cpu_usage",
+                "value": psutil.cpu_percent(interval=None) / 100,
+                "tags": {}
+            }
+        )
+
+    def collect_network_metrics(self) -> None:
+        net = psutil.net_io_counters()
+        self.metrics.extend([
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "net_bytes_sent",
+                "value": net.bytes_sent,
+                "tags": {}
+            },
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "net_bytes_recv",
+                "value": net.bytes_recv,
+                "tags": {}
+            },
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "net_packets_sent",
+                "value": net.packets_sent,
+                "tags": {}
+            },
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "net_packets_recv",
+                "value": net.packets_recv,
+                "tags": {}
+            },
+        ])
+
+
+    def collect_load_average_metrics(self) -> None:
+        load1, load5, load15 = psutil.getloadavg()
+        cpu_count = psutil.cpu_count()
+
+        self.metrics.extend([
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "load_1_norm",
+                "value": load1 / cpu_count,
+                "tags": {}
+            },
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "load_5_norm",
+                "value": load5 / cpu_count,
+                "tags": {}
+            },
+            {
+                "host": settings.host,
+                "vm": VM,
+                "metric": "load_15_norm",
+                "value": load15 / cpu_count,
+                "tags": {}
+            },
+        ])
+
+    def collect_ram_metrics(self) -> None:
+        mem = psutil.virtual_memory()
+        self.metrics.append({
             "host": settings.host,
             "vm": VM,
-            "metric": "net_bytes_sent",
-            "value": net.bytes_sent,
+            "metric": "ram_used_pct",
+            "value": mem.percent / 100,
             "tags": {}
-        },
-        {
+        })
+
+    def collect_disk_metrics(self) -> None:
+        disk = psutil.disk_usage("/")
+        self.metrics.append({
             "host": settings.host,
             "vm": VM,
-            "metric": "net_bytes_recv",
-            "value": net.bytes_recv,
-            "tags": {}
-        },
-        {
-            "host": settings.host,
-            "vm": VM,
-            "metric": "net_packets_sent",
-            "value": net.packets_sent,
-            "tags": {}
-        },
-        {
-            "host": settings.host,
-            "vm": VM,
-            "metric": "net_packets_recv",
-            "value": net.packets_recv,
-            "tags": {}
-        },
-    ])
+            "metric": "disk_used_pct",
+            "value": disk.percent / 100,
+            "tags": {"mount": "/"}
+        })
 
+    def collect(self) -> None:
+        """ Collect system metrics
+        :return:
+        """
+        logger.debug("Collect metrics")
 
-def collect_load_average_metrics(metrics: List[Dict[str, Any]]) -> None:
-    load1, load5, load15 = psutil.getloadavg()
-    cpu_count = psutil.cpu_count()
-
-    metrics.extend([
-        {
-            "host": settings.host,
-            "vm": VM,
-            "metric": "load_1_norm",
-            "value": load1 / cpu_count,
-            "tags": {}
-        },
-        {
-            "host": settings.host,
-            "vm": VM,
-            "metric": "load_5_norm",
-            "value": load5 / cpu_count,
-            "tags": {}
-        },
-        {
-            "host": settings.host,
-            "vm": VM,
-            "metric": "load_15_norm",
-            "value": load15 / cpu_count,
-            "tags": {}
-        },
-    ])
-
-
-def collect_metrics() -> List[Dict[str, Any]]:
-    """ Collect system metrics
-    :return:
-    """
-    logger.debug("Collect metrics")
-    metrics: List[Dict[str, Any]] = [{
-        "host": settings.host,
-        "vm": VM,
-        "metric": "cpu_usage",
-        "value": psutil.cpu_percent(interval=None) / 100,
-        "tags": {}
-    }]
-
-    collect_load_average_metrics(metrics=metrics)
-
-    mem = psutil.virtual_memory()
-    metrics.append({
-        "host": settings.host,
-        "vm": VM,
-        "metric": "ram_used_pct",
-        "value": mem.percent / 100,
-        "tags": {}
-    })
-
-    disk = psutil.disk_usage("/")
-    metrics.append({
-        "host": settings.host,
-        "vm": VM,
-        "metric": "disk_used_pct",
-        "value": disk.percent / 100,
-        "tags": {"mount": "/"}
-    })
-
-    collect_network_metrics(metrics=metrics)
-
-    return metrics
+        self.collect_load_average_metrics()
+        self.collect_load_average_metrics()
+        self.collect_load_average_metrics()
+        self.collect_disk_metrics()
+        self.collect_network_metrics()
 
 
 def send(metrics: List[Dict[str, Any]]):
@@ -128,14 +137,15 @@ def send(metrics: List[Dict[str, Any]]):
 
 
 def main():
+    collector = Collector()
     try:
-        metrics = collect_metrics()
-        if not metrics:
+        collector.collect()
+        if not collector.metrics:
             logger.warning("No metrics collected")
             return
 
-        logger.info(f"Send {len(metrics)} metrics")
-        send(metrics=metrics)
+        logger.info(f"Send {len(collector.metrics)} metrics")
+        send(metrics=collector.metrics)
 
         logger.info("Collector finished successfully")
     except Exception as e:
